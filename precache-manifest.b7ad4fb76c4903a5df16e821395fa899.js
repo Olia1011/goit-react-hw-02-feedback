af2bb72501635a1c2184b1ea447099e9725dd8ff
Olia1011 <olgamelnyk10111989@gmail.com>
2020-05-04 01:30:47 +0300

    Updates

---
+++ precache-manifest.b7ad4fb76c4903a5df16e821395fa899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd5420aa2ad1335698b1797a0fb84f0",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "c2e76b503e9da603897a",
+    "url": "/goit-react-hw-02-feedback/static/css/main.0c66c591.chunk.css"
+  },
+  {
+    "revision": "b35df8aae660bb4d5001",
+    "url": "/goit-react-hw-02-feedback/static/js/2.dc80431d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.dc80431d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e76b503e9da603897a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.ed34ad64.chunk.js"
+  },
+  {
+    "revision": "530a2ea63a63b985e80c",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.f97b67d2.js"
+  }
+]);
